b24d207abdb14d563180505cd000ffb2112ce1d3
melissaceee <cheng.melissa@outlook.com>
2024-09-23 18:11:30 -0400

    changes to bookshelf

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -9,8 +9,13 @@ const Bookshelf = () => {
         setNewBook({...newBook, [event.target.name]: event.target.value});
     };
 
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
     
+        setBooks([...books, newBook]);
+        setNewBook({ title: "", author: "" });
+      };
+      return (
 
 <div className="bookshelfDiv">
   <div className="formDiv">
